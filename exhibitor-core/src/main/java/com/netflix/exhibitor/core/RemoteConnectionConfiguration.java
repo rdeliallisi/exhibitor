0f2619392d4f6fdbff11a75825d75108d1c8e001
Rubin Deliallisi <rdeliallisi@mesosphere.io>
2017-07-28 17:11:58 +0200

    Added SSL support to RemoteConnectionConfiguration without breaking backwards compatibility

--- exhibitor-core/src/main/java/com/netflix/exhibitor/core/RemoteConnectionConfiguration.java
+++ exhibitor-core/src/main/java/com/netflix/exhibitor/core/RemoteConnectionConfiguration.java
@@ -6,44 +6,53 @@ import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.filter.ClientFilter;
 import java.util.Collection;
 
-public class RemoteConnectionConfiguration
-{
+public class RemoteConnectionConfiguration {
     private final Collection<ClientFilter> filters;
     private final int connectionTimeoutMs;
     private final int readTimeoutMs;
+    private final SSLConfigurationBundle sslBundle;
 
     private static final int DEFAULT_CONNECTION_TIMEOUT_MS = 10000;
     private static final int DEFAULT_READ_TIMEOUT_MS = 3000;
 
-    public RemoteConnectionConfiguration()
-    {
-        this(Lists.<ClientFilter>newArrayList(), DEFAULT_CONNECTION_TIMEOUT_MS, DEFAULT_READ_TIMEOUT_MS);
+    public RemoteConnectionConfiguration() {
+        this(new SSLConfigurationBundle(), Lists.<ClientFilter>newArrayList(), DEFAULT_CONNECTION_TIMEOUT_MS,
+                DEFAULT_READ_TIMEOUT_MS);
     }
 
-    public RemoteConnectionConfiguration(Collection<ClientFilter> filters)
-    {
-        this(filters, DEFAULT_CONNECTION_TIMEOUT_MS, DEFAULT_READ_TIMEOUT_MS);
+    public RemoteConnectionConfiguration(Collection<ClientFilter> filters) {
+        this(new SSLConfigurationBundle(), filters, DEFAULT_CONNECTION_TIMEOUT_MS, DEFAULT_READ_TIMEOUT_MS);
     }
 
-    public RemoteConnectionConfiguration(Collection<ClientFilter> filters, int connectionTimeoutMs, int readTimeoutMs)
-    {
+    public RemoteConnectionConfiguration(SSLConfigurationBundle sslBundle) {
+        this(sslBundle, Lists.<ClientFilter>newArrayList(), DEFAULT_CONNECTION_TIMEOUT_MS, DEFAULT_READ_TIMEOUT_MS);
+    }
+
+    public RemoteConnectionConfiguration(SSLConfigurationBundle sslBundle, Collection<ClientFilter> filters) {
+        this(sslBundle, filters, DEFAULT_CONNECTION_TIMEOUT_MS, DEFAULT_READ_TIMEOUT_MS);
+    }
+
+    public RemoteConnectionConfiguration(SSLConfigurationBundle sslBundle, Collection<ClientFilter> filters,
+            int connectionTimeoutMs, int readTimeoutMs) {
+        this.sslBundle = sslBundle;
         this.filters = ImmutableList.copyOf(Preconditions.checkNotNull(filters, "filters cannot be null"));
         this.connectionTimeoutMs = connectionTimeoutMs;
         this.readTimeoutMs = readTimeoutMs;
     }
 
-    public Collection<ClientFilter> getFilters()
-    {
+    public Collection<ClientFilter> getFilters() {
         return filters;
     }
 
-    public int getConnectionTimeoutMs()
-    {
+    public int getConnectionTimeoutMs() {
         return connectionTimeoutMs;
     }
 
-    public int getReadTimeoutMs()
-    {
+    public int getReadTimeoutMs() {
         return readTimeoutMs;
+    }
+
+    public SSLConfigurationBundle getSslBundle() {
+        return sslBundle;
     }
 }
